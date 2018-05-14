ba338eae5cf19ee372fc6053c80607c129aa9f32
Greibus <tonny12sk8@gmail.com>
2018-05-14 15:57:10 -0600

    Agregado cada usuario a el json

--- Server/Servidor.cpp
+++ Server/Servidor.cpp
@@ -8,7 +8,7 @@
 
 
 #include "../../rapidxml/rapidxml.hpp"
-#include "../UserData/LinkedListUser.h"
+#include "../UserData/LinkedListUser.cpp"
 
 using namespace std;
 string prueba = "";
@@ -122,9 +122,9 @@ void *Servidor::hiloConexion(void *socket) {
             string genders = doc.first_node()->first_node()->first_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->value();
             string password = doc.first_node()->first_node()->first_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->value();
             string friends = doc.first_node()->first_node()->first_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->next_attribute()->value();
-            //listaUser.addLast(user, name, lastName, age, genders, password, friends);
-            //jsonUser.push_back(listaUser.toJson());
-            cout << "PRUEBAAAA" << endl;
+            listaUser.addLast(user, name, lastName, age, genders, password, friends);
+            jsonUser.push_back(listaUser.toJson());
+            cout << "JSON USER ES " << jsonUser << endl;
         }
         //Colocar logica para tratar mensajes recibidoa
     }
--- main.cpp
+++ main.cpp
@@ -3,24 +3,15 @@
 #include <fstream>
 #include <vector>
 #include "Server/Servidor.h"
-#include "UserData/LinkedListUser.cpp"
 #include "../../rapidxml/rapidxml.hpp"
 
 
 using namespace std;
 
 int main(){
-
-
-    LinkedListUser<string> listaUser;
     std::cout << "Iniciando Servidor" << std::endl;
-    listaUser.addLast("asd", "asd", "asd", "sda", "asd", "asd", "asd");
-    listaUser.addLast("asd", "asd", "asd", "sda", "asd", "asd", "asd");
-    listaUser.search("asd");
-    listaUser.delUser("asd");
-    listaUser.isEmpty();
-    //Servidor servidor = Servidor();
-    //servidor.iniciar();
+    Servidor servidor = Servidor();
+    servidor.iniciar();
     return 0;
 
 }
