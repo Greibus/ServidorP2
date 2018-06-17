a2f79ba8c30557049c57345a7bf10cb8f26affab
Chanchus <pabloesquivel88@gmail.com>
2018-06-17 15:27:46 -0700

    Update al codigo Huffman

--- HuffmanTestjava/src/HuffmanDecoder.java
+++ HuffmanTestjava/src/HuffmanDecoder.java
@@ -1,7 +1,26 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.*;
 import java.util.Map.Entry;
- 
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
  
 public class HuffmanDecoder {
     // input is an array of frequencies, indexed by character code
@@ -45,7 +64,7 @@ public class HuffmanDecoder {
             codedString+= prefix.toString();
             
             CodeList.put(leaf.value, prefix.toString());
-            // print out character, frequency, and code for this leaf (which is just the prefix)
+            //print out character, frequency, and code for this leaf (which is just the prefix)
             System.out.println(leaf.value + "\t" + leaf.frequency + "\t" + prefix);
  
         } else if (tree instanceof HuffmanNode) {
@@ -79,7 +98,7 @@ public class HuffmanDecoder {
     			
     			tempStr += codedArray[i];
     			
-    			System.out.println("tempStr: " + tempStr);
+    			//System.out.println("tempStr: " + tempStr);
     			
     			for (Entry<Character, String> entry : CodeList.entrySet())
     			{
@@ -106,14 +125,176 @@ public class HuffmanDecoder {
     
     
     
+	public String Encode(String toEncode) {
+		
+		CodeList.clear();
+		
+		System.out.println("String to encode: " + toEncode);
+		
+		// we will assume that all our characters will have
+        // code less than 256, for simplicity
+        int[] charFreqs = new int[256];
+        
+        
+        
+        
+        // read each character and record the frequencies
+        for (char c : toEncode.toCharArray()) {
+            charFreqs[c]++;
+            
+        }
+        
+        HuffmanTree tree = buildTree(charFreqs);
+        
+        // print out results
+        //System.out.println("SYMBOL\tWEIGHT\tHUFFMAN CODE");
+        
+        
+        
+        printCodes(tree, new StringBuffer());
+        
+        
+        
+        String codedString = "";
+        
+        for (char c : toEncode.toCharArray()) {
+            
+        	for (Entry<Character, String> entry : CodeList.entrySet())
+            {
+                if(entry.getKey() == c) {
+                	codedString += entry.getValue();
+                }
+            }
+            
+        }
+        
+        return codedString;
+        
+	}
     
     
     
+	public void xmlToCodes(String xmlString)
+	{
+		
+		CodeList.clear();
+		
+		DocumentBuilder db = null;
+	
+		Document document = null;
+		
+		try {
+			
+			db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			
+			document = db.parse( new InputSource( new StringReader( xmlString ) ) );
+			
+			
+		
+		
+		} catch (SAXException | IOException e) {
+		
+			e.printStackTrace();
+		} catch (ParserConfigurationException e1) {
+			
+			e1.printStackTrace();
+		}
+		
+		Node mainNode = document.getFirstChild();
+		
+		NodeList nodeList = mainNode.getChildNodes();
+	    for (int i = 0; i < nodeList.getLength(); i++) {
+	        Node node = nodeList.item(i);
+	        if (node.getNodeType() == Node.ELEMENT_NODE) {
+	            
+	        	String character = "";
+	        	String code = "";
+	        	
+	        	
+	        	character += node.getNodeName();
+	        	code += node.getTextContent().toString();
+	        	
+	        	
+	        	
+	        	
+	            //System.out.println(node.getNodeName() + ", code: " + node.getTextContent().toString());
+	            CodeList.put(character.charAt(0), code);
+	            
+	        }
+	    }
+		
+	    
+	    for (Entry<Character, String> entry : CodeList.entrySet())
+		{
+		
+			System.out.println("char: " + entry.getKey() + ", code: " + entry.getValue());
+		
+		    
+		}
+		
+		
+	}
     
     
-    
-    
- 
+	public String codesToXml()
+	{
+		 
+		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = null;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			
+			e.printStackTrace();
+		}
+
+		// root element
+		Document document = docBuilder.newDocument();
+		
+		Element rootElement = document.createElement("message");
+		document.appendChild(rootElement);
+		
+		
+		// for each element in CodeList
+		for (Entry<Character, String> entry : CodeList.entrySet())
+		{
+			
+			
+			Element charEle = document.createElement("character");
+			
+			rootElement.appendChild(charEle);
+			
+			Element character = document.createElement("key");
+			character.appendChild(document.createTextNode(entry.getKey().toString()));
+			
+			Element code = document.createElement("code");
+			code.appendChild(document.createTextNode(entry.getValue()));
+			
+			charEle.appendChild(character);
+			charEle.appendChild(code);
+	
+		    
+		}
+		
+		try
+	    {
+	       DOMSource domSource = new DOMSource(document);
+	       StringWriter writer = new StringWriter();
+	       StreamResult result = new StreamResult(writer);
+	       TransformerFactory tf = TransformerFactory.newInstance();
+	       Transformer transformer = tf.newTransformer();
+	       transformer.transform(domSource, result);
+	       return writer.toString();
+	    }
+	    catch(TransformerException ex)
+	    {
+	       ex.printStackTrace();
+	       return null;
+	    }
+		
+		
+	}
     
         
 }
--- HuffmanTestjava/src/Main.java
+++ HuffmanTestjava/src/Main.java
@@ -6,49 +6,16 @@ public class Main {
 	public static void main(String args[])
 	{
 		
-		String test = "<Message><jsonMsg> this is an example for huffman encoding </jsonMsg> </Message>";
+		String test = "<Message><a>101</a> <b>0001</b> <c>1001</c> <d>11</d></Message>";
 		 
-        // we will assume that all our characters will have
-        // code less than 256, for simplicity
-        int[] charFreqs = new int[256];
-        
-        
-        
-        
-        // read each character and record the frequencies
-        for (char c : test.toCharArray()) {
-            charFreqs[c]++;
-            
-        }
+
         
         HuffmanDecoder huffDecoder = new HuffmanDecoder();
- 
-        // build tree
-        HuffmanTree tree = huffDecoder.buildTree(charFreqs);
- 
-        // print out results
-        System.out.println("SYMBOL\tWEIGHT\tHUFFMAN CODE");
+
         
         
         
-        huffDecoder.printCodes(tree, new StringBuffer());
-        
-        
-        
-        String codedString = "";
-        
-        for (char c : test.toCharArray()) {
-            
-        	for (Entry<Character, String> entry : huffDecoder.CodeList.entrySet())
-            {
-                if(entry.getKey() == c) {
-                	codedString += entry.getValue();
-                }
-            }
-            
-        }
-        
-        
+        String codedString = huffDecoder.Encode(test);
         
         
         System.out.println("huffman coded string: " + codedString);
@@ -57,10 +24,24 @@ public class Main {
         System.out.println("Decoded message: " + huffDecoder.Decode(codedString));
         
         
+        System.out.println("codes to xml: " + huffDecoder.codesToXml());
+        
+        
+        huffDecoder.xmlToCodes(test);
+        
+        
+        System.out.println("Decoded message: " + huffDecoder.Decode("1010001100111"));
+        
+        
+        
     }
 		
+	
+
+
+	
+	
 		
 }
-	
 	
 
