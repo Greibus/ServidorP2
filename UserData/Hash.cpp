7cb5e08b45f11968a473ec7be6dbf906e8260278
Kisung Lim <kisungqueso@hotmail.com>
2018-06-18 01:03:21 -0600

    Actualizacion del hash

--- UserData/Hash.cpp
+++ UserData/Hash.cpp
@@ -10,5 +10,8 @@ std::string Hash::hash(std::string code) {
     for (int i = 0; i < size ; i+=2){
         hashed += code[i];
     }
+    for (int i = 1; i < size ; i+=2){
+        hashed += code[i];
+    }
     return hashed;
 }
